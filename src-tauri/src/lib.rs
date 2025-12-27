8deb492514aec2806b7df4e173f9bd713d2c3c41
wanger <310779211wym@gmail.com>
2025-12-28 01:47:02 +0800

    from begining

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,10 +8,14 @@ mod types;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    std::env::set_var("NO_PROXY", "localhost,127.0.0.1");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(commands::handlers())
         .setup(|app| {
+            #[cfg(debug_assertions)]
+            app.get_webview_window("main").unwrap().open_devtools();
             app.manage(app_state::AppState::new());
             Ok(())
         })
